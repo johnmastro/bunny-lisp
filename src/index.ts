92a73800f31bd99eb3aa66298d1d0e15e6166161
John Mastro <john.b.mastro@gmail.com>
2023-12-11 08:10:18 -0500

    make repl sync

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,29 @@ import { VirtualMachine } from "./virtual-machine/virtual-machine.ts";
 import { readString } from "./reader/reader.ts";
 import * as printer from "./printer.ts";
 
-async function repl(): Promise<void> {
+function repl(): void {
     const vm = new VirtualMachine();
-    process.stdout.write(">>> ");
-    for await (const line of console) {
-        if (line === ":exit") {
-            return;
+    for (;;) {
+        const input = prompt(">>>");
+        if (!input) {
+            continue;
         }
-        if (line !== "") {
-            try {
-                const form = readString(line);
-                const thunk = vm.compileThunk(form);
-                const result = vm.apply(thunk, []);
-                printer.printLn(result);
-            } catch (e) {
-                if (e instanceof Error) {
-                    process.stdout.write(`${e.name}: ${e.message}\n`);
-                } else {
-                    process.stdout.write(`${String(e)}\n`);
-                }
-                vm.reset();
+        if (input === ":exit") {
+            break;
+        }
+        try {
+            const form = readString(input);
+            const thunk = vm.compileThunk(form);
+            const result = vm.apply(thunk, []);
+            printer.printLn(result);
+        } catch (e) {
+            if (e instanceof Error) {
+                process.stdout.write(`${e.name}: ${e.message}\n`);
+            } else {
+                process.stdout.write(`${String(e)}\n`);
             }
+            vm.reset();
         }
-        process.stdout.write(">>> ");
     }
 }
 
